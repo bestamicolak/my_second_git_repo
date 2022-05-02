15d14d7a1d0fd8ac3c68c0cbc793a441bb809f83
Bestami Colak <colaksaglam@gmail.com>
2022-05-02 16:10:01 +0100

    Trial 4

--- src/RemoteJavaClass03.java
+++
@@ -1,6 +0,0 @@
-            public class RemoteJavaClass03.java {
-    public static void main(String[] args) {
-        System.out.println("hello 3");
-    }
-
-}
--- src/RemoteJavaClass04.java
+++
@@ -1,6 +0,0 @@
-            public class RemoteJavaClass04.java {
-    public static void main(String[] args) {
-        System.out.println("hello 4");
-    }
-
-}
